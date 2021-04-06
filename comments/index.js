395cf14e3cbab29087826c2006fd2a53dc767dc8
mani <manibboyz@gmail.com>
2021-04-06 19:33:09 +0530

    Updating Comment Content

--- comments/index.js
+++ comments/index.js
@@ -32,13 +32,35 @@ app.post('/posts/:id/comments', async (req, res) => {
             postId: req.params.id,
             status: 'pending'
         }
-    })
+    });
 
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Received Event', req.body.type);
+
+    const { type, data } = req.body;
+
+    if(type === "CommentModerated") {
+        const { id, postId, status, content } = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+
+        await axios.post('http://localhost:4005', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                status,
+                content
+            }
+        });
+    }
 
     res.send({});
 });
