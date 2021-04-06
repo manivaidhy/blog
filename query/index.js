bdd3ba81fd7f60282e877b57bdceca1e68cf648e
mani <manibboyz@gmail.com>
2021-04-06 20:43:25 +0530

    misc updates

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const BodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(BodyParser.json());
